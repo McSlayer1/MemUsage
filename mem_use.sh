7ded9bb9c811f2f38f8afef9b5bc56505323a12e
Michael <collin.salyer@gmail.com>
2018-03-30 17:03:56 +0000

    Added another method of reading the process memory usage!

--- mem_use.sh
+++ mem_use.sh
@@ -9,11 +9,13 @@ bash -c "exec $cmd > /dev/null 2>&1 &"
 temp=`ps -ef | grep -P "$cmd"` #| awk '{print $2}'`
 IFS=' ' read -r -a array <<< "$temp"
 PID="${array[1]}"
-mem=`awk 'BEGIN { used=0 }; /Rss/ { used += $2 } END { print used }' /proc/$PID/smaps`
+mem1=`awk 'BEGIN { used=0 }; /Rss/ { used += $2 } END { print used }' /proc/$PID/smaps`
+mem2=`pmap -x $PID | grep -i total | awk '{print $4}'`
 time2=`date +%s%6N`
 used2=`free | grep Mem | awk '{print $3}'`
 echo "PID: $PID"
 echo "time: $[time2 - time1]us"
-echo "Mem: $mem kB"
+echo "Mem: $mem1 kB"
+echo "Mem: $mem2 kB"
 #pkill -f cmd_proc
 pkill -f "$cmd"
